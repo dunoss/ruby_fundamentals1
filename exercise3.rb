9719c0072d54b8260bbcf8dcb01bc71ffab56ea2
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-27 21:23:12 -0400

    how old are you

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
 puts "What is your name?"
 name = gets
 puts "Hi #{name}!"
+
+#puts "What year were you born?"
+#year = gets
+#puts "This year you are #{2017 - year.to_i} years old"
+#testing ^
+
+puts "How old are you?"
+age = gets
+puts "you were born in #{2017 - age.to_i}"
