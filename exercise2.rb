27159231ff2c6e0adf27b992942e2f633418af72
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-27 20:23:58 -0400

    ex2

---
+++ exercise2.rb
@@ -0,0 +1,26 @@
+#How would you calculate a good tip for a 55 dollar meal?
+#Use puts to print the answer.
+puts 55 * 1.3
+
+#Try adding a string and an integer with the + operator.
+#What happens? Find a way to convert the integer into a string
+#first and use puts to print the result.
+
+puts "I\'m #{2017 - 1986} years old"
+
+puts "What\'s 1 + 1?"
+puts 1 + 1
+
+#Try outputting the result of 45628 multiplied by 7839 in a
+#sentence by using string interpolation.
+
+puts "45628 X 7839 = #{45628 * 7839}"
+
+#What's the value of the expression
+#(10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on
+#your own before typing it in.
+
+# (True & False) or True
+# my answer = True
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
